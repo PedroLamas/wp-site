50b0661ec92173a2e99b0f823f5e2796b7d07597
Pedro Lamas <pedrolamas@gmail.com>
2012-08-30 18:20:50 +0100

    Fixed the time string on top of the screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 jQuery(document).ready(function() {
 	var updateTime = function() {
 		var dt = new Date();
+		var h = dt.getHours();
+		var m = dt.getMinutes();
 		
-		$("#time").text(dt.getHours() + ":" + dt.getMinutes());
+		$("#time").text(h + ":" + (m <= 9 ? "0" : "") + m);
 		
 		window.setTimeout(updateTime, 1000);
 	};
